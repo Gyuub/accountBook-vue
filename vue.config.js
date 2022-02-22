02c02956328500eebd33711e9759646f388eccd0
Gyub <hman0301@kakao.com>
2022-02-22 20:01:17 +0900

    개발환경, disableHostCheck 속성 추가

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: process.env.VUE_APP_BASE_URL,
+  //publicPath: process.env.VUE_APP_BASE_URL,
 
   "devServer": {
-    overlay: false
+    overlay: false,
+    disableHostCheck: true, // [WDS] Disconnected! 작성된 포트 번호와 실제 구동한 포트번호의 불일치.
   },
   "transpileDependencies": [
     "vuetify"
